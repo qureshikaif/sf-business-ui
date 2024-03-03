e37860e1fd2ef0b88fa6566b7691b5981eb85a0c
qureshikaif <qureshik488@gmail.com>
2024-03-03 20:28:50 +0500

    fix: fixed responsiveness on about page

--- src/components/MeetUs.tsx
+++ src/components/MeetUs.tsx
@@ -2,10 +2,26 @@ import Image from "next/image";
 import Four from "../../public/images/four.jpg";
 import { Button } from "./ui/button";
 import { Parallax } from "react-scroll-parallax";
+import Link from "next/link";
+
+const meetus = [
+  {
+    image: Four,
+    name: "Abdus Samad Roomie",
+    role: "CEO of SF Business Solutions",
+    linkedin: "/",
+  },
+  {
+    image: Four,
+    name: "Faiq Hussain",
+    role: "CTO of SF Business Solutions",
+    linkedin: "/",
+  },
+];
 
 const MeetUs = () => {
   return (
-    <section className="bg-blue-700 h-screen overflow-hidden flex flex-col items-center justify-center relative">
+    <section className="bg-blue-700 lg:h-screen overflow-hidden flex flex-col items-center justify-center relative">
       <Parallax
         className="absolute -top-24 -right-24"
         speed={-20}
@@ -26,27 +42,30 @@ const MeetUs = () => {
             your business challenges.
           </p>
         </div>
-        <div className="grid grid-cols-4 gap-5">
-          <div className="flex flex-col items-center space-y-4">
-            <Image
-              src={Four}
-              alt="Test"
-              className="aspect-square rounded-lg object-cover"
-            />
-            <h1 className="text-white text-lg font-semibold">
-              Abdus Samad Roomie
-            </h1>
-            <p className="text-white">CEO of SF Business Solutions</p>
-          </div>
-          <div className="flex flex-col items-center space-y-4">
-            <Image
-              src={Four}
-              alt="Test"
-              className="aspect-square rounded-lg object-cover"
-            />
-            <h1 className="text-white text-lg font-semibold">Faiq Hussain</h1>
-            <p className="text-white">CTO of SF Business Solutions</p>
-          </div>
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5">
+          {meetus.map((member, index) => (
+            <div key={index} className="flex flex-col items-center space-y-4">
+              <div className="relative group">
+                <Image
+                  src={member.image}
+                  alt={member.name}
+                  className="aspect-square rounded-lg object-cover"
+                />
+                <Link href={member.linkedin}>
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="absolute inset-0 bg-blue-500 opacity-0 group-hover:opacity-70"></div>
+                    <p className="text-white text-lg font-semibold z-10 opacity-0 group-hover:opacity-100 ">
+                      LinkedIn
+                    </p>
+                  </div>
+                </Link>
+              </div>
+              <h1 className="text-white text-lg font-semibold">
+                {member.name}
+              </h1>
+              <p className="text-white">{member.role}</p>
+            </div>
+          ))}
         </div>
         <div className="flex items-center justify-center py-10">
           <Button className="bg-blue-500">Book a consultation</Button>
